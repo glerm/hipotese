626ccc2dae66a98a39d8b5f512157d4d05e1f811
Glerm Soares <organismo@gmail.com>
2025-08-07 17:30:57 -0300

    Update responses.js elabore

--- responses.js
+++ responses.js
@@ -1,6 +1,6 @@
 // Objeto com todas as respostas do terminal
 const responses = {
     'continue': 'a <a href="index_velho.html">chave</a> para desvendar os mistérios que nos cercam.',
-    'elabore': 'Elaborando o pensamento... A complexidade reside na simplicidade. Quando examinamos atentamente, encontramos padrões onde antes só víamos caos.',
+    'elabore': 'Quando examinamos atentamente, encontramos padrões onde antes só víamos caos.',
     'conclua': 'não seja o esperado, cada conclusão é um novo ponto de partida.'
 };
