3b78995d3e0a01fd729805d39d52c84266d73d89
Glerm Soares <organismo@gmail.com>
2025-08-13 09:34:39 -0300

    Update responses.js sem acento

--- responses.js
+++ responses.js
@@ -3,5 +3,5 @@ const responses = {
     'continue': 'a <a href="index_velho.html">chave</a> para desvendar os mistérios que nos cercam.',
     'elabore': 'Quando examinamos atentamente, encontramos padrões onde antes só víamos caos.',
     'conclua': 'não seja o esperado, cada conclusão é um novo ponto de partida.',
-    'códigos': '<a href="https://github.com/glerm/hipotese/">fontes</a> para novas hipóteses'
+    'codigos': '<a href="https://github.com/glerm/hipotese/">fontes</a> para novas hipóteses'
 };
