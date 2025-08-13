859b84b63314782d438d483b8caf33e9bf15b79e
Glerm Soares <organismo@gmail.com>
2025-08-13 09:57:18 -0300

    Update responses.js rascunhos link

--- responses.js
+++ responses.js
@@ -4,5 +4,5 @@ const responses = {
     'elabore': 'Quando examinamos atentamente, encontramos padrões onde antes só víamos caos.',
     'conclua': 'não seja o esperado, cada conclusão é um novo ponto de partida.',
     'codigos': '<a href="https://github.com/glerm/hipotese/">Fontes</a> para novas hipóteses'
-    'rascunhos': '<a href="https://github.com/glerm/hipotese/">Rascunhos</a> do experimento pedagógico'
+    'rascunhos': '<a href="https://scratch.mit.edu/studios/36506201/">Rascunhos</a> do experimento pedagógico'
 };
