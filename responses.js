d46f5a3bf36d327ec767d3a11071d1d51c671cd7
Glerm Soares <organismo@gmail.com>
2025-08-13 15:39:37 -0300

    Update responses.js computer flusser

--- responses.js
+++ responses.js
@@ -3,6 +3,6 @@ const responses = {
     'continue': 'a <a href="index_velho.html">chave</a> para desvendar os mistérios que nos cercam.',
     'elabore': 'Quando examinamos atentamente, encontramos padrões onde antes só víamos caos.',
     'conclua': 'não seja o esperado, cada conclusão é um novo ponto de partida.',
-    'codigos': '<a href="https://github.com/glerm/hipotese/">Fontes</a> para novas hipóteses',
+    'codigos': '<a href="https://github.com/glerm/hipotese/">Fontes</a> para novas hipóteses <br> <img src="computer_flusser.jpg" width="400" >',
     'rascunhos': '<a href="https://scratch.mit.edu/studios/36506201/">Rascunhos</a> do experimento pedagógico. <br> <img src="CENAPIXELART.jpg" width="320" >'
 };
